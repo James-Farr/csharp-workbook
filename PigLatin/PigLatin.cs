6bf269b4bdbb041169e4997d6f0f64c43e20980a
James Farr <jtfarr@farrwestenv.com>
2018-08-06 10:15:14 -0500

    Upload of tictaktoe

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -7,11 +7,14 @@ namespace PigLatin
         public static void Main()
         {
             // your code goes here
-
+        
             // leave this command at the end so your program does not close automatically
             Console.ReadLine();
         }
         
+        void test() {
+            Console.WriteLine("test");
+        }
         public static string TranslateWord(string word)
         {
             // your code goes here
